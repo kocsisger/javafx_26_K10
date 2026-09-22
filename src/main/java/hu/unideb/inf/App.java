e803ec75c640b9b89fc7ce530a9c45833b357b07
Kocsis Gergely <kocsisg@example.com>
2026-09-22 11:09:28 +0200

    Fourth commit

--- src/main/java/hu/unideb/inf/App.java
+++ src/main/java/hu/unideb/inf/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
         //First change
         //Second change
         //Third change
+        //Fourth change
     }
 
     static void setRoot(String fxml) throws IOException {
